b85a669b351c163f96dd0c93917eed04a3204ed2
jero2 <rachel990210@gmail.com>
2021-04-11 19:14:45 +0900

    01 [+] boosted

---
+++ README.md
@@ -0,0 +1,10 @@
+### Boosted website [*Clone Coding*]
+
+----------------
+Boosted website의 'main page'와 'electric skateboards(sub page)'를 
+클론코딩 하였습니다.
+
+
+```
+메인컬러가 black 과 red로 이루어져 있어 레이아웃이 깔끔하게 눈에 들어와 선택하였습니다.
+```
---
+++ js/dino-common.js
@@ -0,0 +1,18 @@
+// common script
+
+$(document).ready(function() {
+  preventDefaultAnchor();
+
+});
+
+function preventDefaultAnchor() {
+  $(document).on('click', 'a[href="#"]', function(e) {
+    e.preventDefault();
+  });
+}
+
+
+
+
+
+
